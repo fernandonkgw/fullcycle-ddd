dbcbcd1a9f972faa61f694d93fa4aec668328f17
Fernando Nakagawa <fernandonkgw@gmail.com>
2022-07-07 14:12:42 -0300

    customer notification pattern

--- src/domain/customer/entity/customer.ts
+++ src/domain/customer/entity/customer.ts
@@ -1,17 +1,24 @@
+import Entity from "../../@shared/entity/entity.abstract";
+import NotificationError from "../../@shared/notification/notification.error";
 import Address from "../value-object/address";
 
-export default class Customer {
+export default class Customer extends Entity{
 
-    private _id: string;
     private _name: string = "";
     private _address!: Address;
     private _active: boolean = false;
     private _rewardPoints: number = 0;
 
     constructor(id: string, name: string) {
+        super();
         this._id = id;
         this._name = name;
         this.validate();
+
+        if (this.notification.hasErrors()) {
+            throw new NotificationError(this.notification.getErrors());
+            
+        }
     }
 
     get id(): string {
@@ -31,11 +38,17 @@ export default class Customer {
     }
 
     validate() {
-        if (this._id.length === 0) {
-            throw new Error("Id is required");
+        if (this.id.length === 0) {
+            this.notification.addError({
+                context: "customer",
+                message: "Id is required",
+            });
         }
         if (this._name.length === 0) {
-            throw new Error("Name is required");
+            this.notification.addError({
+                context: "customer",
+                message: "Name is required",
+            });
         }
     }
 
